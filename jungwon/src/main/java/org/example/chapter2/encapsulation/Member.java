78e1f58b8f260933b32306b3ee6fd3db739a63a0
jungwon <mac2001035@gmail.com>
2024-05-06 01:13:43 +0900

    FEAT: 캡슐화 적용

--- jungwon/src/main/java/org/example/chapter2/encapsulation/Member.java
+++ jungwon/src/main/java/org/example/chapter2/encapsulation/Member.java
@@ -5,7 +5,17 @@ import java.util.Date;
 public class Member {
     private Date expiryDate;
     private boolean male;
+    long day30 = 1000 * 60 *60 *24 * 30;
 
+    public boolean isExpired(){
+        if (male) {
+            return expiryDate != null
+                    && expiryDate.getDate() < System.currentTimeMillis();
+        }
+
+        return expiryDate !=null &&
+                expiryDate.getDate() <System.currentTimeMillis()-day30;
+    }
 
     public Date getExpiryDate(){
         return expiryDate;
---
+++ jungwon/src/main/java/org/example/chapter2/encapsulation/ServiceV2.java
@@ -0,0 +1,11 @@
+package org.example.chapter2.encapsulation;
+
+public class ServiceV2 {
+
+    public void expired(Member member){
+
+        if(member.isExpired()){
+            //만료 처리
+        }
+    }
+}
