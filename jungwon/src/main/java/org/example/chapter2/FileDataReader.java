b0b60e06f6f21f61e65833ab07da88afbbf511c4
jungwon <mac2001035@gmail.com>
2024-05-07 18:56:10 +0900

    FEAT: 기존 코드

--- jungwon/src/main/java/org/example/chapter2/FileDataReader.java
+++ jungwon/src/main/java/org/example/chapter2/FileDataReader.java
@@ -3,6 +3,9 @@ package org.example.chapter2;
 public class FileDataReader {
     private String fileName;
 
+    public FileDataReader() {
+    }
+
     public FileDataReader(String fileName) {
         this.fileName = fileName;
     }
