6b8450ffb6c3cc7c3b06d014ddbaa5376e0f632c
jungwon <mac2001035@gmail.com>
2024-05-12 20:50:43 +0900

    FEAT: 전략 패턴 구현 전.

---
+++ jungwon/src/main/java/org/example/chapter7/Item.java
@@ -0,0 +1,14 @@
+package org.example.chapter7;
+
+public class Item {
+    int price;
+    boolean fresh;
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public boolean isFresh() {
+        return this.fresh;
+    }
+}
