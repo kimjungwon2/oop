127088ccba6b6e7c927c3f8a3a062f931169a84d
jungwon <mac2001035@gmail.com>
2024-05-13 18:15:48 +0900

    FEAT: 아이템별로 전략 패턴 적용.

--- jungwon/src/main/java/org/example/chapter7/DiscountStrategy.java
+++ jungwon/src/main/java/org/example/chapter7/DiscountStrategy.java
@@ -2,4 +2,5 @@ package org.example.chapter7;
 
 public interface DiscountStrategy {
     int getDiscountPrice(Item item);
+    int getDiscountPrice(int totalPrice);
 }
--- jungwon/src/main/java/org/example/chapter7/FirstDiscountStrategy.java
+++ jungwon/src/main/java/org/example/chapter7/FirstDiscountStrategy.java
@@ -5,4 +5,9 @@ public class FirstDiscountStrategy implements DiscountStrategy{
     public int getDiscountPrice(Item item) {
         return (int) (item.getPrice() * 0.9);
     }
+
+    @Override
+    public int getDiscountPrice(int totalPrice) {
+        return (int)(totalPrice * 0.9);
+    }
 }
